0eddf459a6ad9527f9895bc5d188a4ec984e6843
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-10-03 12:42:26 -0600

    Added javadoc documentation to PopupDisplay

--- src/lists/view/PopupDisplay.java
+++ src/lists/view/PopupDisplay.java
@@ -1,14 +1,28 @@
 package lists.view;
 
 import javax.swing.JOptionPane;
-
+/**
+ * 
+ * Simple GUI class using JOptionPane
+ * @author Derek Vawdrey
+ *@version 1.0 03.10.2017
+ */
 public class PopupDisplay
 {
+	/**
+	 * Displays a popup showing the supplied String to the user.
+	 * @param message The text to be displayed.
+	 */
 	public void displayText(String message)
 	{
 		JOptionPane.showMessageDialog(null, message);
 	}
-
+	
+	/**
+	 * Displays a popup to get a response from the user.
+	 * @param fromQuestion The text to be displayed
+	 * @return The users response to the question
+	 */
 	public String getResponse(String fromQuestion)
 	{
 		String answer = "";
